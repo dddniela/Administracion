e3bf9b657510fd772ec29eef3884a186d3c1d341
DanielaMendezAr <danielamendez0809@gmail.com>
2023-06-19 16:22:53 -0600

    update trazado titulos

--- src/View/perfilEgreso-vista.php
+++ src/View/perfilEgreso-vista.php
@@ -4,8 +4,8 @@
         <img class="w-100 img-fluid" src="img/portadas/IMG_ECO1821.png" alt="">
         <div class="position-absolute top-50 start-50 translate-middle w-100">
             <div class="d-flex flex-column justify-content-center align-items-center text-center">
-                <h1 class="fw-bold text-warning">Perfil de Egreso</h1>
-                <h1 class="fw-bold text-light d-md-flex d-none">Licenciatura en Administración</h1>
+                <h1 class="fw-bold text-warning text-stroke">Perfil de Egreso</h1>
+                <h1 class="fw-bold text-light d-md-flex d-none text-stroke">Licenciatura en Administración</h1>
             </div>
         </div>
     </div>
